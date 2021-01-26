9db7c44f0d1608c1899c5a6f851f433b93a41fa9
Johannes Poehn <johannes.poehn@gmail.com>
2021-01-27 00:25:30 +0100

    Console Double Buffering

---
+++ ElmishPokemon/Playground.fsx
@@ -0,0 +1,321 @@
+﻿//open Elmish
+//open System
+
+//module Counter =
+
+//    type Model = { 
+//        name : string
+//        count : int
+//    }
+
+//    type Msg =
+//        | Increment
+//        | Decrement
+
+//    let init name msg =
+//        { name = name; count = 0 }, msg
+
+//    let update msg model =
+//        match msg with
+//        | Increment -> 
+//            let newCount = model.count + 1
+//            printfn $"Incrementing {model.name}... New Count: {newCount}"
+//            { model with count = newCount }, 
+//            match newCount with
+//            | x when x > 9 -> Cmd.none
+//            | _ -> Cmd.ofMsg Increment
+        
+//        | Decrement -> 
+//            let newCount = model.count - 1
+//            printfn $"Decrementing {model.name}... New Count: {newCount}"
+//            { model with count = newCount },
+//            match newCount with
+//            | x when x < -9 -> Cmd.none
+//            | _ -> Cmd.ofMsg Decrement
+
+//module Main =
+
+//    type Model = { 
+//        top : Counter.Model
+//        bottom : Counter.Model 
+//    }
+
+//    type Msg =
+//        | Reset
+//        | Top of Counter.Msg
+//        | Bottom of Counter.Msg
+
+//    let init() =
+//        let top, topCmd = Cmd.ofMsg Counter.Increment |> Counter.init "top"
+//        let bottom, bottomCmd = Cmd.ofMsg Counter.Decrement |> Counter.init "bottom"
+//        printfn $"topCmd is {topCmd}"
+//        printfn $"bottomCmd is {bottomCmd}"
+//        { top = top
+//          bottom = bottom }, 
+//        Cmd.batch [ Cmd.map Top topCmd
+//                    Cmd.map Bottom bottomCmd ]
+
+//    let update msg model : Model * Cmd<Msg> =
+//        printfn $"msg is {msg}"
+//        match msg with
+//        | Reset -> init()
+//        | Top msg' ->
+//            let res, cmd = Counter.update msg' model.top
+//            { model with top = res }, Cmd.map Top cmd
+//        | Bottom msg' ->
+//            let res, cmd = Counter.update msg' model.bottom
+//            { model with bottom = res }, Cmd.map Bottom cmd
+
+//    let view model _ =
+//        printf "%A\n" model
+
+////Program.mkProgram Main.init Main.update Main.view
+////|> Program.run
+
+//let timerAsync (duration:int) =
+//    async {
+//        do! Async.Sleep duration
+//    }
+
+//module Second =
+//    type Msg =
+//        | Second of int
+
+//    type Model = int
+
+//    let subscribe initial =
+//        let sub dispatch =
+//            Async.RunSynchronously (timerAsync 1000)
+//            dispatch (Second System.DateTime.Now.Second) |> ignore
+//        Cmd.ofSub sub
+
+//    let init () =
+//        0
+
+//    let update (Second seconds) model =
+//        seconds
+
+//module Minute =
+//    type Msg =
+//        | Minute of int
+
+//    type Model = int
+
+//    let init () =
+//        0
+
+//    let update (Minute minutes) model =
+//        minutes
+
+//    let subscribe initial =
+//        let sub dispatch =
+//            Async.RunSynchronously (timerAsync 5000)
+//            dispatch (Minute System.DateTime.Now.Minute) |> ignore
+//        Cmd.ofSub sub
+
+//module App =
+//    type Model = { 
+//        seconds : Second.Model
+//        minutes : Minute.Model 
+//    }
+
+//    type Msg =
+//        | SecondMsg of Second.Msg
+//        | MinuteMsg of Minute.Msg
+
+//    let init () = {
+//        seconds = Second.init()
+//        minutes = Minute.init()
+//    }
+
+//    let update msg model =
+//        match msg with
+//        | MinuteMsg msg ->
+//           { model with minutes = Minute.update msg model.minutes }
+//        | SecondMsg msg ->
+//            { model with seconds = Second.update msg model.seconds }
+
+//    let subscription model =
+//        Cmd.batch [ 
+//            Cmd.map SecondMsg (Second.subscribe model.seconds)
+//            Cmd.map MinuteMsg (Minute.subscribe model.minutes)
+//        ]
+
+////Program.mkSimple App.init App.update (fun model _ -> printf "%A\n" model)
+////|> Program.withSubscription App.subscription
+////|> Program.run
+
+
+
+//// Utility functions
+//let tryParseInt (str: string) =
+//    match System.Int32.TryParse str with
+//    | (false, _) -> None
+//    | (true, int) -> Some int
+
+//let loadData (apiKey: string) =
+//    async {
+//        do! Async.Sleep 1000
+//        return [ "This"; "Is"; "Obviously"; "Some"; "Fake"; "Data" ]
+//    }   
+
+//// Model
+//type State =
+//    | PromptForApiKey
+//    | ChooseDatum of data: string list
+//    | DisplayDatum of datum: string
+
+//type Model = {
+//    IsLoading: bool
+//    ApiKey: string option
+//    State: State
+//}
+
+//let init () =
+//    { IsLoading = false; ApiKey = None; State = PromptForApiKey }, Cmd.none
+
+//type Message =
+//    | ApiKeyEntered of apiKey: string
+//    | DataLoaded of data: string list
+//    | DatumChosen of datum: string
+//    | InvalidInput
+    
+//// View
+//let viewApiKeyPrompt model dispatch =
+//    printf "Enter your api key: "
+//    System.Console.ReadLine()
+//    |> ApiKeyEntered |> dispatch
+
+//let viewDataChooser (model, data) dispatch =
+//    let dataMap = data |> List.mapi (fun i datum -> (i + 1), datum) |> Map.ofList
+
+//    printfn "Available data"
+//    dataMap |> Map.iter (fun key datum ->
+//        printfn "- [%i]: %s" key datum
+//    )
+
+//    printf "Choose a datum: "
+    
+//    System.Console.ReadLine()
+//    |> tryParseInt
+//    |> Option.bind (fun key -> Map.tryFind key dataMap)
+//    |> function
+//        | Some datum -> DatumChosen datum |> dispatch
+//        | None -> InvalidInput |> dispatch
+
+//let viewDatum (model, datum) dispatch =
+//    printfn "You've chosen %s" datum
+    
+//    printfn "Press enter to exit."
+//    System.Console.ReadLine() |> ignore
+
+//let view model dispatch =
+//    System.Console.Clear()
+//    if model.IsLoading = true then
+//        printfn "Loading"
+
+//    match model.State with
+//    | PromptForApiKey -> viewApiKeyPrompt model dispatch
+//    | ChooseDatum data -> viewDataChooser (model, data) dispatch
+//    | DisplayDatum datum -> viewDatum (model, datum) dispatch
+
+//// Update
+//let update message model =
+//    match model.State, message with
+//    | _, InvalidInput -> { model with IsLoading = false }, Cmd.none
+
+//    | PromptForApiKey, ApiKeyEntered apiKey ->
+//        { model with IsLoading = true; ApiKey = Some apiKey },
+//        Cmd.OfAsync.perform loadData apiKey DataLoaded
+    
+//    | PromptForApiKey, DataLoaded data ->
+//        { model with State = ChooseDatum data; IsLoading = false }, Cmd.none
+
+//    | ChooseDatum _, DatumChosen datum ->
+//        { model with State = DisplayDatum datum }, Cmd.none
+
+//    | _ -> model, Cmd.none
+
+////[<EntryPoint>]
+////let main argv =
+////    Program.mkProgram init update view
+////    |> Program.run
+////    0
+
+//module Program =
+//    /// <summary>
+//    /// Program with user-defined orders instead of usual command.
+//    /// Orders are processed by <code>execute</code> which can dispatch messages,
+//    /// called in place of usual command processing.
+//    /// </summary>
+//    let mkProgramWithOrderExecute
+//            (init: 'arg' -> 'model * 'order)
+//            (update: 'msg -> 'model -> 'model * 'order)
+//            (view: 'model -> Dispatch<'msg> -> 'view)
+//            (execute: 'order -> Dispatch<'msg> -> unit) =
+//        let convert (model, order) = 
+//            model, order |> execute |> Cmd.ofSub 
+//        Program.mkProgram
+//            (init >> convert)
+//            (fun msg model -> update msg model |> convert)
+//            view
+
+////let createConsoleScreenBuffer = 
+////    CreateConsoleScreenBuffer(
+////        GENERIC_READ ||| GENERIC_WRITE,
+////        FILE_SHARE_READ ||| FILE_SHARE_WRITE,
+////        IntPtr.Zero,
+////        CONSOLE_TEXTMODE_BUFFER,
+////        IntPtr.Zero)
+
+////[<DllImport("kernel32.dll", SetLastError = true)>]
+////extern IntPtr GetStdHandle(
+////    int nStdHandle);
+
+////let STD_OUTPUT_HANDLE = -11
+
+////let getStdHandle = 
+////    GetStdHandle(STD_OUTPUT_HANDLE)
+
+////[<DllImport("kernel32.dll", SetLastError = true)>]
+////extern bool SetConsoleActiveScreenBuffer(
+////    IntPtr hConsoleOutput);
+
+////let setConsoleActiveScreenBuffer hConsoleOutput =
+////    SetConsoleActiveScreenBuffer(hConsoleOutput)
+////    Console.
+
+////[<type:StructLayout(LayoutKind.Sequential)>]
+////type Coord = {
+////    X : int16
+////    Y : int16
+////}
+
+////[<type:StructLayout(LayoutKind.Explicit, CharSet=CharSet.Unicode)>]
+////type CharUnion = {
+////    [<FieldOffset(0)>] UnicodeChar : char
+////    [<FieldOffset(0)>] AsciiChar : byte
+////}
+
+////[<type:StructLayout(LayoutKind.Explicit)>]
+////type CharInfo = {
+////    [<FieldOffset(0)>] Char : CharUnion
+////    [<FieldOffset(2)>] Attributes : int16
+////}
+
+////[<type:StructLayout(LayoutKind.Sequential)>]
+////type SmallRect = {
+////    Left : int16
+////    Top : int16
+////    Right : int16
+////    Bottom : int16
+////}
+
+////[<DllImport("kernel32.dll", SetLastError = true, EntryPoint = "WriteConsoleOutputW")>]
+////extern bool WriteConsoleOutputW(
+////    IntPtr hConsoleOutput, 
+////    CharInfo[] lpBuffer, 
+////    Coord dwBufferSize, 
+////    Coord dwBufferCoord, 
+////    SmallRect lpWriteRegion);
+
--- ElmishPokemon/Program.fs
+++ ElmishPokemon/Program.fs
@@ -1,8 +1,159 @@
-﻿// Learn more about F# at http://fsharp.org
-
+﻿open Elmish
 open System
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+module Program =
+    // http://fssnip.net/80F
+    let mkProgramWithOrderExecute
+            (init: 'arg' -> 'model * 'order)
+            (update: 'msg -> 'model -> 'model * 'order)
+            (view: 'model -> Dispatch<'msg> -> 'view)
+            (execute: 'order -> Dispatch<'msg> -> unit) =
+        let convert (model, order) = 
+            model, order |> execute |> Cmd.ofSub 
+        Program.mkProgram
+            (init >> convert)
+            (fun msg model -> update msg model |> convert)
+            view
+            
+
+module ConsoleProgram = 
+    type console = {
+        handle : CsWin32.Handle
+        line : int
+    }
+
+    let stdHandle = CsWin32.Kernel32.GetStdHandle()
+    let newHandle = CsWin32.Kernel32.CreateConsoleScreenBuffer()
+
+    let setCursorInvisible console = 
+        CsWin32.Kernel32.SetCursorInvisible console.handle
+        console
+
+    let setActiveConsole console = 
+        CsWin32.Kernel32.SetConsoleActiveScreenBuffer(console.handle) |> ignore
+        console
+
+    let clearOutput console =
+        CsWin32.Kernel32.ClearScreen console.handle
+        { console with line = 0 }
+
+    let writeLineToOutput line console =
+        CsWin32.Kernel32.WriteConsoleOutput(console.handle, line, 0, console.line, line.Length, 1) |> ignore
+        { console with line = console.line + 1 }
+
+    let rec writeLinesToOutput (lines : string list) console = 
+        match lines with
+        | [] -> console
+        | x::xs -> 
+            console
+            |> writeLineToOutput x
+            |> writeLinesToOutput xs 
+
+    let writeTextToOutput (text : string) console = 
+        writeLinesToOutput (text.Split '\n' |> Array.toList) console
+    
+    let writeDescription console =
+        console |> writeTextToOutput "Count-o-matic : watch integers being counted on your console.
+Press Space to start, pause or resume.
+Press Enter to step while in pause.
+Press +/- to increase/decrease speed while running.v
+Press Q to quit."
+
+    type Model = { 
+        Running: bool
+        Count: int
+        Interval: int 
+        FrontBuffer: console
+        BackBuffer: console
+    }
+
+    let printModel model console = 
+        console
+        |> clearOutput
+        |> writeDescription
+        |> writeLineToOutput $"{model.Count}"
+        |> setActiveConsole
+        |> ignore
+
+    let switchBuffers model = 
+        { model with FrontBuffer = model.BackBuffer; BackBuffer = model.FrontBuffer }
+    let increaseCount model =
+        { model with Count = model.Count + 1 }
+    let setRunning model =
+        { model with Running = true }
+    let setPaused model =
+        { model with Running = false }
+    let changeInterval x model = 
+        { model with Interval = model.Interval+x |> min 2500 |> max 50 }
+
+    type Msg = 
+        | TimerTick
+        | KeyboardTick
+        | Toggle
+        | ChangeInterval of offset: int
+    /// user-defined order type
+    type Order =
+        | StartKeyListener
+        | Print of value: Model
+        | DelayTick of delay: int
+        | CancelDelayedTick
+        | Orders of Order list
+        | NoOrder
+
+    let init (running, interval) =
+        //let backBuffer = 
+        let model = { 
+            Running = running
+            Count = 0
+            Interval = interval
+            FrontBuffer = { handle = newHandle; line = 0 } |> setCursorInvisible |> setActiveConsole |> writeDescription
+            BackBuffer = { handle = newHandle; line = 0 } |> setCursorInvisible
+        }
+        model, Orders [ StartKeyListener ; if running then DelayTick 0 ]
+
+    let update msg model =
+        match msg, model.Running with
+        | TimerTick, true
+        | Toggle, false ->
+            let model' = model |> setRunning |> increaseCount |> switchBuffers
+            model', Orders [ Print model' ; DelayTick model.Interval ]
+        | Toggle, true ->
+            model |> setPaused, CancelDelayedTick
+        | KeyboardTick, false -> 
+            let model' = model |> increaseCount |> switchBuffers
+            model', Print model'
+        | ChangeInterval x, true ->
+            model |> changeInterval x, NoOrder
+        | KeyboardTick, true | ChangeInterval _, false | TimerTick, false -> 
+            model, NoOrder
+
+    let view _ _ = ()
+    
+    /// Function executing orders, with a dispatch function as second argument.
+    let rec execute order dispatch =
+        match order with
+        | StartKeyListener ->
+            async {
+                seq { while true do (Console.ReadKey true).KeyChar }
+                |> Seq.takeWhile (fun key -> key <> 'q' && key <> 'Q')  // press q to quit
+                |> Seq.iter (function
+                    | ' ' -> dispatch Toggle
+                    | '\013' -> dispatch KeyboardTick   // Enter key
+                    | '-' -> dispatch (ChangeInterval 50)
+                    | '+' -> dispatch (ChangeInterval -50)
+                    | _ -> ())
+                Async.CancelDefaultToken () }
+            |> Async.StartImmediate
+        | Print model -> 
+            model.FrontBuffer
+            |> printModel model
+        | DelayTick delay ->
+            async { do! Async.Sleep delay
+                    dispatch TimerTick }
+            |> Async.Start
+        | CancelDelayedTick -> Async.CancelDefaultToken ()
+        | Orders orders -> for order in orders do execute order dispatch
+        | NoOrder -> ()
+
+Program.mkProgramWithOrderExecute ConsoleProgram.init ConsoleProgram.update ConsoleProgram.view ConsoleProgram.execute
+|> Program.runWith (false, 350)
